5564ed95177f12fd85fc34a4ca5e23160dd62e70
Kehinde - David Damilare Samuel <64425915+Qui-vira@users.noreply.github.com>
2022-11-27 11:14:41 +0100

    Create storegeh.sol
    This is a Solidity Extension that stores the name and number of catbreeds.

---
+++ storegeh.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity <=0.8.7;
+
+contract catstore {
+   
+   mapping(string=>uint256) public nameofcatbreedstonoofcatbreeds;
+
+    
+    uint256  noofcatbreeds;
+    
+    Vetshop [] public vetshop;
+
+    struct Vetshop {
+        uint256 noofcatbreeds;
+        string nameofcatbreeds;
+    }
+
+    function storenoofcatbreeds(uint256 _noofcatbreeds) public virtual  {
+        noofcatbreeds = _noofcatbreeds;
+    }
+
+    function readnoofcatbreeds() public view returns (uint256) {
+        return noofcatbreeds;
+    }
+
+    function catbreedsgrouped (string memory _nameofcatbreeds,  uint256 _noofcatbreeds) public {
+    Vetshop memory newcatbreedsgrouped = Vetshop ({noofcatbreeds: _noofcatbreeds, nameofcatbreeds: _nameofcatbreeds});
+    vetshop.push(newcatbreedsgrouped);
+    nameofcatbreedstonoofcatbreeds[_nameofcatbreeds] = _noofcatbreeds;
+    
+    }
+
+
+    
+}
